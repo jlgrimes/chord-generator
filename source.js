7f1c1b94c7da1ece8f8c7d0cb81f314d23101bf7
Jared Grimes <jaredlancegrimes@gmail.com>
2017-02-25 18:27:08 -0500

    Added option to change key for chords

--- source.js
+++ source.js
@@ -1,73 +1,363 @@
+var key = "C";
+
 function major()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "E4", "G4", "C5"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "E4", "G4", "C5"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E#4", "G#4", "C#5"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F#4", "A4", "D5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "G4", "A#4", "D#5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G#4", "B4", "E5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "A4", "C5", "F5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A#4", "C#5", "F#5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "B4", "D5", "G5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "C5", "D#5", "G#5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C#5", "E5", "A5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "D5", "E#5", "A#5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D#5", "F#5", "B5"], "2n");
 }
 
 function minor()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "G4", "C5"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "G4", "C5"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E4", "G#4", "C#5"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F4", "A4", "D5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "Gb4", "A#4", "D#5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G4", "B4", "E5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "Ab4", "C5", "F5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A4", "C#5", "F#5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "Bb4", "D5", "G5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "Cb5", "D#5", "G#5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C5", "E5", "A5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "Db5", "E#5", "A#5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D5", "F#5", "B5"], "2n");
 }
 
 function diminished()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "Gb4", "C5"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "Gb4", "C5"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E4", "G4", "C#5"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F4", "Ab4", "D5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "Gb4", "A4", "D#5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G4", "Bb4", "E5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "Ab4", "Cb5", "F5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A4", "C5", "F#5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "Bb4", "Db5", "G5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "Cb5", "D5", "G#5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C5", "Eb5", "A5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "Db5", "E5", "A#5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D5", "F5", "B5"], "2n");
 }
 
 function augmented()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "E4", "G#4", "C5"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "E4", "G#4", "C5"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E#4", "A4", "C#5"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F#4", "A#4", "D5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "G4", "B4", "D#5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G#4", "C5", "E5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "A4", "C#5", "F5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A#4", "D5", "F#5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "B4", "D#5", "G5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "C5", "E5", "G#5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C#5", "E#5", "A5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "D5", "F#5", "A#5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D#5", "G5", "B5"], "2n");
 }
 
 function major7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "E4", "G4", "B4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "E4", "G4", "B4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E#4", "G#4", "C5"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F#4", "A4", "Db5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "G4", "A#4", "D5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G#4", "B4", "Eb5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "A4", "C5", "Fb5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A#4", "C#5", "F5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "B4", "D5", "Gb5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "C5", "D#5", "G5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C#5", "E5", "Ab5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "D5", "E#5", "A5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D#5", "F#5", "Bb5"], "2n");
 }
 
 function minor7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "G4", "Bb4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "G4", "Bb4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E4", "G#4", "B4"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F4", "A4", "C5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "Gb4", "A#4", "C#5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G4", "B4", "D5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "Ab4", "C5", "D#5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A4", "C#5", "E5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "Bb4", "D5", "F5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "Cb5", "D#5", "F#5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C5", "E5", "G5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "Db5", "E#5", "G#5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D5", "F#5", "A5"], "2n");
 }
 
 function diminished7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "Gb4", "A4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "Gb4", "A4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E4", "G4", "Bb4"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F4", "Ab4", "B4"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "Gb4", "A4", "C5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G4", "Bb4", "Db5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "Ab4", "B4", "D5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A4", "C5", "Eb5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "Bb4", "Db5", "Fb5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "Cb5", "D5", "F5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C5", "Eb5", "Gb5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "Db5", "E5", "G5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D5", "F5", "Ab5"], "2n");
 }
 
 function halfdiminished7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "Gb4", "Bb4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "Gb4", "Bb4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E4", "G4", "B4"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F4", "Ab4", "C5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "Gb4", "A4", "C#5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G4", "Bb4", "D5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "Ab4", "B4", "D#5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A4", "C5", "E5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "Bb4", "Db5", "F5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "Cb5", "D5", "F#5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C5", "Eb5", "G5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "Db5", "E5", "G#5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D5", "F5", "A5"], "2n");
 }
 
 function dominant7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "E4", "G4", "Bb4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "E4", "G4", "Bb4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E#4", "G#4", "B4"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F#4", "A4", "C5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "G4", "A#4", "Db5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G#4", "B4", "D5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "A4", "C5", "Eb5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A#4", "C#5", "E5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "B4", "D5", "F5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "C5", "D#5", "Gb5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C#5", "E5", "G5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "D5", "E#5", "Ab5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D#5", "F#5", "A5"], "2n");
 }
 
 function augmentedmajor7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "E4", "G#4", "B4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "E4", "G#4", "B4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E#4", "A4", "C5"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F#4", "A#4", "Db5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "G4", "B4", "D5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G#4", "C5", "Eb5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "A4", "C#5", "Fb5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A#4", "D5", "F5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "B4", "D#5", "Gb5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "C5", "E5", "G5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C#5", "E#5", "Ab5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "D5", "F#5", "A5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D#5", "G5", "Bb5"], "2n");
 }
 
 function augmentedminor7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "E4", "G#4", "Bb4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "E4", "G#4", "Bb4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E#4", "A4", "B4"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F#4", "A#4", "C5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "G4", "B4", "Db5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G#4", "C5", "D5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "A4", "C#5", "Eb5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A#4", "D5", "E5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "B4", "D#5", "F5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "C5", "E5", "Gb5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C#5", "E#5", "G5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "D5", "F#5", "Ab5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D#5", "G5", "A5"], "2n");
 }
 
 function minormajor7th()
 {
     var polySynth = new Tone.PolySynth(5, Tone.Synth).toMaster();
-    polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "G4", "B4"], "2n")
+
+    if (key == 'C')
+        polySynth.triggerAttackRelease(["C3", "C4", "Eb4", "G4", "B4"], "2n");
+    else if (key == 'C#')
+        polySynth.triggerAttackRelease(["C#3", "C#4", "E4", "G#4", "C5"], "2n");
+    else if (key == 'D')
+        polySynth.triggerAttackRelease(["D3", "D4", "F4", "A4", "C#5"], "2n");
+    else if (key == 'D#')
+        polySynth.triggerAttackRelease(["D#3", "D#4", "Gb4", "A#4", "D5"], "2n");
+    else if (key == 'E')
+        polySynth.triggerAttackRelease(["E3", "E4", "G4", "B4", "D#5"], "2n");
+    else if (key == 'F')
+        polySynth.triggerAttackRelease(["F3", "F4", "Ab4", "C5", "E5"], "2n");
+    else if (key == 'F#')
+        polySynth.triggerAttackRelease(["F#3", "F#4", "A4", "C#5", "E#5"], "2n");
+    else if (key == 'G')
+        polySynth.triggerAttackRelease(["G3", "G4", "Bb4", "D5", "F#5"], "2n");
+    else if (key == 'G#')
+        polySynth.triggerAttackRelease(["G#3", "G#4", "Cb5", "D#5", "G5"], "2n");
+    else if (key == 'A')
+        polySynth.triggerAttackRelease(["A3", "A4", "C5", "E5", "G#5"], "2n");
+    else if (key == 'A#')
+        polySynth.triggerAttackRelease(["A#3", "A#4", "Db5", "E#5", "A5"], "2n");
+    else if (key == 'B')
+        polySynth.triggerAttackRelease(["B3", "B4", "D5", "F#5", "A#5"], "2n");
 }
 
 function I(){
@@ -262,7 +552,7 @@ function V_vii() {
     //play a chord
 
     polySynth.triggerAttackRelease(["A2", "A3", "C4", "E4", "A4"], "2n");
-    polySynth.triggerAttackRelease(["F2", "F4", "A4", "C5", "Eb5"], "2n", "+2n");
+    polySynth.triggerAttackRelease(["F2", "F4", "C4", "Eb4", "A4"], "2n", "+2n");
     polySynth.triggerAttackRelease(["B2", "B3", "D4", "F4", "B4"], "2n", "+1m");
     polySynth.triggerAttackRelease(["C3", "C4", "E4", "G4", "C5"], "2n", "+(1m + 2n)")
 }
